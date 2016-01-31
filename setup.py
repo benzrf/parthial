7bc486b4fd9b484d5ddd6c2e12d80ac472aa64b5
benzrf <benzrf@benzrf.com>
2016-01-31 02:34:08 -0500

    Made a setup.py!

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="parthial",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["camel"],
+
+    author="benzrf",
+    author_email="benzrf@benzrf.com",
+    description="Lisp interpreter for user-scriptable"
+                "server-side applications",
+    long_description=open("README.rst").read(),
+    url="https://github.com/benzrf/parthial",
+    license='GPLv3',
+    zip_safe=True,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Lisp',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Interpreters',
+    ],
+)
+
