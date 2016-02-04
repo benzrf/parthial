781fe2e4a9870d7d1117f9c9cf20a026e9921b9d
benzrf <benzrf@benzrf.com>
2016-02-03 21:53:36 -0500

    Redid errs.py docstrings for sphinx.

--- parthial/errs.py
+++ parthial/errs.py
@@ -3,7 +3,11 @@ A hierarchy of exceptions that may be raised by the interpreter.
 """
 
 class LispError(Exception):
-    """Base class for interpreter errors."""
+    """Base class for interpreter errors.
+
+    Attributes:
+        val: The primary value involved in the error.
+    """
 
     template = '{}'
     """A template for use in generating error messages."""
@@ -12,19 +16,35 @@ class LispError(Exception):
         self.val = val
 
     def message(self):
-        """Return a user-readable description of the error."""
+        """
+        Returns:
+            A user-readable description of the error.
+        """
         return self.template.format(self.val)
 
 class LimitationError(LispError):
-    """Expression violated evaluation limitations."""
+    """Expression violated evaluation limitations.
+
+    Attributes:
+        val (str): The error message.
+    """
 
 class LispNameError(LispError):
-    """Nonexistent variable."""
+    """Nonexistent variable.
+
+    Attributes:
+        val (str): The name of the nonexistent variable.
+    """
 
     template = 'nonexistent variable {!r}'
 
 class LispTypeError(LispError):
-    """Operation is undefined on given value's type."""
+    """Operation is undefined on given value's type.
+
+    Attributes:
+        val (LispVal): The given value.
+        ex (type or str): The expected type.
+    """
 
     def __init__(self, val, ex):
         self.val, self.ex = val, ex
@@ -37,7 +57,12 @@ class LispTypeError(LispError):
             format(ex, self.val.type_name, self.val)
 
 class LispArgTypeError(LispTypeError):
-    """Argument is of the wrong type."""
+    """Argument is of the wrong type.
+
+    Attributes:
+        f (callable): The function that was called.
+        arg (int): The argument number.
+    """
 
     def __init__(self, f, val, ex, arg):
         self.f, self.val, self.ex, self.arg = f, val, ex, arg
@@ -47,14 +72,25 @@ class LispArgTypeError(LispTypeError):
             format(self.arg, self.f, super().message())
 
 class UncallableError(LispTypeError):
-    """Applied value is uncallable."""
+    """Applied value is uncallable.
+
+    Attributes:
+        val (LispVal): The value that was called.
+    """
 
     def __init__(self, val):
         self.val = val
         self.ex = 'a callable'
 
 class ArgCountError(LispTypeError):
-    """Wrong number of args."""
+    """Wrong number of args.
+
+    Attributes:
+        f (callable): The function that was called.
+        got (int): The number of arguments given.
+        ex (int): The expected number of arguments.
+                  Defaults to ``len(f.pars)``.
+    """
 
     def __init__(self, f, got, ex=None):
         if not ex:
